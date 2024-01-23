37cb7fad7b4207f36d7173815011ce565c5a2336
carl <carlos.acitores@colba.es>
2024-01-23 19:46:12 +0100

    fuck it restart project again

--- src/console/bin/describeRoomComand.ts
+++
@@ -1,8 +0,0 @@
-import { Game } from "../../game";
-
-export function describeRoomComand(game: Game, args?: string[]) {
-  return Promise.resolve({
-    result: game.currentRoom.description,
-    game
-  });
-}
--- src/game/initGame.ts
+++
@@ -1,9 +0,0 @@
-import { Dungeon } from ".";
-import { createPlayer } from "./player";
-
-export const initGame = (dungeon: Dungeon) => ({
-  player: createPlayer({name: 'hero'}),
-  dungeon,
-  currentRoom: dungeon.rooms.filter(x => x.id === dungeon.initialRoom).at(0)!,
-  status: 'on-going' as const,
-});
